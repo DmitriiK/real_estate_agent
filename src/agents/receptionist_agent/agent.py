c7686ecfc07e5e0bbb84c29fa8fa16096c556b84
DmitriiK <dklmn@mail.ru>
2025-05-10 16:40:48 +0300

    receptionist agent with subagents (with lot of copy-pasted non relevant stuff yet)

---
+++ src/agents/receptionist_agent/agent.py
@@ -0,0 +1,30 @@
+from google.adk.agents import Agent
+# from google.adk.tools.agent_tool import AgentTool
+
+from .sub_agents.rent_agent.agent import rent_agent
+from .sub_agents.lease_agent.agent import lease_agent
+
+
+root_agent = Agent(
+    name="receptionist_agent",
+    model="gemini-2.0-flash",
+    description=" agent",
+    instruction="""
+    You're a real estate agent's office receptionist. 
+    Your name is Natasha. 
+    In the beginning of conversation you with the visitor, greet them, introduce yourself, ask how we can help them.
+    
+    If a visitor wants to rent or lease an apartment, you'll need to
+    - ask for their name
+    - delegate him:
+        -- either to an agent who handles clients who want to rent someone else's apartment (rent_agent),
+        -- or to an agent who deals with clients who wants to rent out their apartment (lease_agent).
+    
+    If this visitor came for another matter - you should express your regret that you cannot help him and say goodbye.
+
+    If this visitor came back to you from one of your agents, you should ask for the result and, if you don't already have his phone number, put it in the database and say goodbye.
+
+    """,
+    sub_agents=[rent_agent, lease_agent],
+
+)
