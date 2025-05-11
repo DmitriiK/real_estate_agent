7028e08a422b9eda19bff7d8ef50dfb565576254
DmitriiK <dklmn@mail.ru>
2025-05-11 17:43:51 +0300

    parsing of sql to test valid sql

--- src/tests/test_agents.py
+++ src/tests/test_agents.py
@@ -1,4 +1,8 @@
 import unittest
+import re
+
+import sqlglot
+
 from src.agents.agent_runner import run_agent
 from src.agents.text2SQL.agent import root_agent as ag_t2s
 
@@ -6,9 +10,12 @@ class TestAgent(unittest.TestCase):
 
     def test_text2sql_agent(self):
         user_message = "what is the average price to rent something in Antalya?"
-        # initial_state = { "user_name": "Brandon Hancock",}
-        final_responses, final_session_state = run_agent(ag_t2s, user_message)
-        print(final_responses)
+        final_responses, _ = run_agent(ag_t2s, user_message)
+        sql_query = final_responses[0]
+        # Strip out formatting like ```sql and ```, - I could not make llm model produce it without this formatting
+        sql_query = re.sub(r"```.*?\n|```", "", sql_query) 
+        print(sql_query)
+        sqlglot.parse_one(sql_query, read="postgres")
         assert True
 
 if __name__ == "__main__":
